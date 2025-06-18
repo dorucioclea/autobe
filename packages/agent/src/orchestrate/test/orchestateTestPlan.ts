a1e1c1a5216e8f050dadd68959f80292a4b8f946
kakasoo <kscodebase@gmail.com>
2025-06-18 16:37:00 +0900

    feat: add `orchestrateTesrtPlan` draft file

---
+++ packages/agent/src/orchestrate/test/orchestateTestPlan.ts
@@ -0,0 +1,42 @@
+import { IAgenticaHistoryJson, MicroAgentica } from "@agentica/core";
+import { ILlmSchema } from "@samchon/openapi";
+import { v4 } from "uuid";
+
+import { AutoBeContext } from "../../context/AutoBeContext";
+import { enforceToolCall } from "../../utils/enforceToolCall";
+
+export async function orchestrateTestPlan<Model extends ILlmSchema.Model>(
+  ctx: AutoBeContext<Model>,
+) {
+  const operations = ctx.state().interface?.document.operations ?? [];
+  if (operations.length === 0) {
+    throw new Error(
+      "Cannot write test scenarios because these are no operations.",
+    );
+  }
+
+  const agentica: MicroAgentica<Model> = new MicroAgentica({
+    model: ctx.model,
+    vendor: ctx.vendor,
+    config: {
+      ...(ctx.config ?? {}),
+      executor: {
+        describe: null,
+      },
+    },
+    histories: [createHistoryProperties()],
+    controllers: [],
+  });
+  enforceToolCall(agentica);
+
+  const response = await agentica.conversate("");
+}
+
+const createHistoryProperties = () =>
+  ({
+    id: v4(),
+    created_at: new Date().toISOString(),
+    type: "systemMessage",
+    text: "",
+    // text: AutoBeSystemPromptConstant.TEST_PLAN,
+  }) satisfies IAgenticaHistoryJson.ISystemMessage;
