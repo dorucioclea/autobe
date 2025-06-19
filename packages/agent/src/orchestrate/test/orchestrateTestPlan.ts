46a8257a8233227129612a03d4521286221ee64a
kakasoo <kscodebase@gmail.com>
2025-06-19 14:42:14 +0900

    feat: Add logic for sequential test plan generation

--- packages/agent/src/orchestrate/test/orchestrateTestPlan.ts
+++ packages/agent/src/orchestrate/test/orchestrateTestPlan.ts
@@ -6,7 +6,7 @@ import {
 import { AutoBeOpenApi } from "@autobe/interface";
 import { AutoBeTestPlanEvent } from "@autobe/interface/src/events/AutoBeTestPlanEvent";
 import { IAutoBeTestPlan } from "@autobe/interface/src/test/AutoBeTestPlan";
-import { ILlmApplication, ILlmSchema } from "@samchon/openapi";
+import { ILlmApplication, ILlmSchema, IValidation } from "@samchon/openapi";
 import { IPointer } from "tstl";
 import typia from "typia";
 import { v4 } from "uuid";
@@ -26,6 +26,35 @@ export async function orchestrateTestPlan<Model extends ILlmSchema.Model>(
     );
   }
 
+  const exclude: IAutoBeTestPlan.IPlanGroup[] = [];
+  let include: AutoBeOpenApi.IOperation[] = Array.from(operations);
+
+  do {
+    exclude.push(...(await execute(ctx, operations, include, exclude)));
+    include = include.filter((op) => {
+      if (
+        exclude.some((pg) => pg.method === op.method && pg.path === op.path)
+      ) {
+        return false;
+      }
+      return true;
+    });
+  } while (include.length > 0);
+
+  return {
+    type: "testPlan",
+    step: ctx.state().analyze?.step ?? 0,
+    planGroups: exclude,
+    created_at: new Date().toISOString(),
+  } as AutoBeTestPlanEvent;
+}
+
+const execute = async <Model extends ILlmSchema.Model>(
+  ctx: AutoBeContext<Model>,
+  ops: AutoBeOpenApi.IOperation[],
+  include: Pick<AutoBeOpenApi.IOperation, "method" | "path">[],
+  exclude: Pick<AutoBeOpenApi.IOperation, "method" | "path">[],
+) => {
   const pointer: IPointer<IAutoBeTestPlan.IPlanGroup[]> = {
     value: [],
   };
@@ -57,15 +86,14 @@ export async function orchestrateTestPlan<Model extends ILlmSchema.Model>(
     throw new Error("Failed to create test plans.");
   }
 
-  return {
-    type: "testPlan",
-    step: ctx.state().analyze?.step ?? 0,
-    planGroups: pointer.value,
-    created_at: new Date().toISOString(),
-  } as AutoBeTestPlanEvent;
-}
+  return pointer.value;
+};
 
-const createHistoryProperties = (operations: AutoBeOpenApi.IOperation[]) => [
+const createHistoryProperties = (
+  operations: AutoBeOpenApi.IOperation[],
+  include: Pick<AutoBeOpenApi.IOperation, "method" | "path">[],
+  exclude: Pick<AutoBeOpenApi.IOperation, "method" | "path">[],
+) => [
   {
     id: v4(),
     created_at: new Date().toISOString(),
@@ -83,8 +111,33 @@ const createHistoryProperties = (operations: AutoBeOpenApi.IOperation[]) => [
       "However, rather than meaningless tests, business logic tests should be written and an E2E test situation should be assumed.",
       "",
       "```json",
-      JSON.stringify(operations),
+      JSON.stringify(
+        operations.map((el) => ({
+          path: el.path,
+          method: el.method,
+          summary: el.summary,
+        })),
+      ),
       "```",
+    ].join("\n"),
+  } satisfies IAgenticaHistoryJson.ISystemMessage,
+  {
+    id: v4(),
+    created_at: new Date().toISOString(),
+    type: "systemMessage",
+    text: [
+      "# Included in Test Plan",
+      include
+        .map((el) => `- ${el.method.toUpperCase()}: ${el.path}`)
+        .join("\n"),
+      "",
+      "# Excluded from Test Plan",
+      "These are the endpoints that have already been used in test codes generated as part of a plan group.",
+      "These endpoints do not need to be tested again.",
+      "However, it is allowed to reference or depend on these endpoints when writing test codes for other purposes.",
+      exclude
+        .map((el) => `- ${el.method.toUpperCase()}: ${el.path}`)
+        .join("\n"),
     ].join("\n"),
   } satisfies IAgenticaHistoryJson.ISystemMessage,
 ];
@@ -98,6 +151,58 @@ function createApplication<Model extends ILlmSchema.Model>(props: {
   const application: ILlmApplication<Model> = collection[
     props.model
   ] as unknown as ILlmApplication<Model>;
+
+  application.functions[0].validate = (next: unknown): IValidation => {
+    const result: IValidation<IMakePlanProps> =
+      typia.validate<IMakePlanProps>(next);
+    if (result.success === false) return result;
+
+    const errors: IValidation.IError[] = [];
+    result.data.planGroups.forEach((pg, i, arr) => {
+      arr.forEach((target, j) => {
+        if (i !== j && target.method === pg.method && target.path === pg.path) {
+          if (
+            !errors.some(
+              (el) =>
+                el.path !== `planGroups[${j}].path` && el.value !== target.path,
+            )
+          ) {
+            errors.push({
+              path: `planGroups[${j}].path`,
+              expected: `planGroup's {method + path} cannot duplicated.`,
+              value: target.path,
+            });
+          }
+
+          if (
+            !errors.some(
+              (el) =>
+                el.path !== `planGroups[${j}].method` &&
+                el.value !== target.method,
+            )
+          ) {
+            errors.push({
+              path: `planGroups[${j}].method`,
+              expected: `planGroup's {method + path} cannot duplicated.`,
+              value: target.method,
+            });
+          }
+        }
+      });
+    });
+
+    if (errors.length !== 0) {
+      console.log(JSON.stringify(errors, null, 2), "errors");
+      return {
+        success: false,
+        errors,
+        data: next,
+      };
+    }
+
+    return result;
+  };
+
   return {
     protocol: "class",
     name: "Make test plans",
