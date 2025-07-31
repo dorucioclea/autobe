a9b59f364d060e76b77d4a7a35e43d7ca7668e8c
Jeongho Nam <samchon.github@gmail.com>
2025-07-31 17:20:20 +0900

    feat(test): archive realize histories (#292)

--- packages/utils/src/realize/AutoBeRealizeAuthorizationFileSystem.ts
+++
@@ -1,9 +0,0 @@
-export namespace AutoBeRealizeAuthorizationFileSystem {
-  export const getDecoratorPath = (name: string) => `src/decorators/${name}.ts`;
-
-  export const getPayloadPath = (name: string) =>
-    `src/decorators/payload/${name}.ts`;
-
-  export const getProviderPath = (name: string) =>
-    `src/providers/authorize/${name}.ts`;
-}
