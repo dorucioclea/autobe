3d2ad8dbe0b9a9a6ecafef503443135130f1d17b
sunrabbit123 <qudwls185@naver.com>
2025-08-18 15:11:22 +0900

    fix(vscode-extension): reorder number formatting units for compact representation
    - Adjusted the order of number formatting units in toCompactNumberFormat function to ensure correct display of large numbers.
    - Moved trillion (T), billion (B), million (M), and thousand (K) units to follow a more logical sequence.
    
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- packages/vscode-extension/webview-ui/src/util/number.ts
+++ packages/vscode-extension/webview-ui/src/util/number.ts
@@ -1,10 +1,10 @@
 export const toCompactNumberFormat = (value: number) => {
   const units = [
-    { value: 1_000, symbol: "K" },
-    { value: 1_000_000, symbol: "M" },
-    { value: 1_000_000_000, symbol: "B" },
-    { value: 1_000_000_000_000, symbol: "T" },
     { value: 1_000_000_000_000_000, symbol: "Q" },
+    { value: 1_000_000_000_000, symbol: "T" },
+    { value: 1_000_000_000, symbol: "B" },
+    { value: 1_000_000, symbol: "M" },
+    { value: 1_000, symbol: "K" },
   ];
 
   for (const unit of units) {
