0d7c6c17b6231f76444726b9c03a8bb665b9d1ae
sunrabbit123 <qudwls185@naver.com>
2025-08-28 16:34:46 +0900

    fix(ui): correct import paths in upload components
    - Updated import statement in AutoBeChatUploadBox to use relative path correctly.
    - Modified index file to export AutoBeChatUploadSendButton instead of AutoBeFileUploadBox for accurate component accessibility.

--- packages/ui/src/upload/AutoBeChatUploadBox.tsx
+++ packages/ui/src/upload/AutoBeChatUploadBox.tsx
@@ -10,7 +10,7 @@ import {
   AutoBeChatUploadSendButton,
   AutoBeFileUploadBox,
   AutoBeVoiceRecoderButton,
-} from "..";
+} from ".";
 import { AutoBeFileUploader } from "../utils";
 
 export interface IAutoBeBucket {
--- packages/ui/src/upload/index.ts
+++ packages/ui/src/upload/index.ts
@@ -1,4 +1,4 @@
 export * from "./AutoBeFileUploadBox";
 export * from "./AutoBeVoiceRecoderButton";
 export * from "./AutoBeChatUploadBox";
-export * from "./AutoBeFileUploadBox";
+export * from "./AutoBeChatUploadSendButton";
