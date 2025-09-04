002aa16bcd5108c2b5f59a7ae98e7390ef7ae135
seungwoo <sam@wrtn.io>
2025-09-04 15:53:47 +0900

    Update next.config.ts

--- website/next.config.ts
+++ website/next.config.ts
@@ -7,7 +7,7 @@ const withNextra = nextra({
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
   // ... Other Next.js config options
-  basePath: "/autobe",
+  basePath: "/",
   output: "export",
   trailingSlash: true,
   images: {
